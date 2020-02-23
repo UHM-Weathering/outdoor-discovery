947894cf041ee1cc7009dcf580f3d70d70879e71
Emily Pang <42831699+eppang@users.noreply.github.com>
2020-02-22 23:50:13 -1000

    Cleaned up login.php

--- pages/login.php
+++ pages/login.php
@@ -3,21 +3,39 @@
 $title = "Login";
 
 $html = <<<END
-<div class="ui container">
-  <form class="ui form" action="post">
-    <div class="field">
-      <label for="email_input">Email</label>
-      <input id="email_input" type="text" name="email" placeholder="Email">
-    </div>
-    <input type="hidden" name="action" value="process">
-    <div class="field">
-      <label for="password_input">Password: </label>
-      <input id="password_input" type="password" name="password" placeholder="Password">
+<style type=text/css>
+  .column {
+    max-width: 450px;
+  }
+</style>
 
-    </div>
-    <button class="ui button" type="submit">Submit</button>
-  </form>
+  <div class="ui middle aligned center aligned grid">
+  <div class="column">
+    <h2 class="ui blue header">
+      <div class="content">Login</div>
+    </h2>
+    <form class="ui large form" action="post">
+      <div class="ui segment">
+        <div class="field">
+          <div class="ui left icon input">
+            <i class="user icon"></i>
+            <input id="email_input" type="text" name="email" placeholder="Email">
+          </div>
+        </div>
+        <input type="hidden" name="action" value="process">
+        <div class="field">
+          <div class="ui left icon input">
+            <i class="lock icon"></i>
+            <input id="password_input" type="password" name="password" placeholder="Password">
+          </div>
+        </div>
+        <button class="ui fluid large blue submit button button" type="submit">Submit</button>
+      </div>
+
+    </form>
+  </div>
 </div>
+
 END;
 ?>
 
