79c00ee4e3e2c8e349fe497a5fe62421828d4712
Kahlin Baughman <kahlin@hawaii.edu>
2020-03-15 19:56:20 -1000

    Edit and Add Event files

---
+++ pages/addevent.php
@@ -0,0 +1,144 @@
+<?php
+$page_title = "Create Event";
+
+$html = <<<END
+
+<div class="ui container" style="padding-top: 10px">
+    <form method="post" action="" class="ui form">
+      <h2 class="ui dividing header">Event Information</h2>
+      <div class="fields">
+            <div class="six wide field">
+                <label>Name</label>
+                <input type="text" name="event-name" placeholder="Event Name" maxLength="64">
+            </div>
+            <div class="six wide field">
+                <label>Event Location</label>
+                <input type="text" name="event-location" placeholder="Event Location" maxLength="64">
+            </div>
+            <div class="six wide field">
+                <label>Max Participants</label>
+                <input type="text" name="max-participants" placeholder="Max Number of Participants" maxLength="10">
+            </div>
+      </div>
+      <div class="wide field">
+      <label>Event Start</label>
+        <div class="two fields">
+          <div class="field">
+            <select class="ui fluid search dropdown" name="start-month">
+              <option value="1">January</option>
+              <option value="2">February</option>
+              <option value="3">March</option>
+              <option value="4">April</option>
+              <option value="5">May</option>
+              <option value="6">June</option>
+              <option value="7">July</option>
+              <option value="8">August</option>
+              <option value="9">September</option>
+              <option value="10">October</option>
+              <option value="11">November</option>
+              <option value="12">December</option>
+            </select>
+          </div>
+          <div class="field">
+            <select class="ui fluid search dropdown" name="start-day">
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              <option value="5">5</option>
+              <option value="6">6</option>
+              <option value="7">7</option>
+              <option value="8">8</option>
+              <option value="9">9</option>
+              <option value="10">10</option>
+              <option value="11">11</option>
+              <option value="12">12</option>
+              <option value="13">13</option>
+              <option value="14">14</option>
+              <option value="15">15</option>
+              <option value="16">16</option>
+              <option value="17">17</option>
+              <option value="19">19</option>
+              <option value="20">20</option>
+              <option value="21">21</option>
+              <option value="22">22</option>
+              <option value="23">23</option>
+              <option value="24">24</option>
+              <option value="25">25</option>
+              <option value="26">26</option>
+              <option value="27">27</option>
+              <option value="28">28</option>
+              <option value="29">29</option>
+              <option value="30">30</option>
+              <option value="31">31</option>
+            </select>
+          </div>
+        </div>
+      </div>
+      <div class="wide field">
+        <label>Event End</label>
+        <div class="two fields">
+          <div class="field">
+            <select class="ui fluid search dropdown" name="end-month">
+              <option value="1">January</option>
+              <option value="2">February</option>
+              <option value="3">March</option>
+              <option value="4">April</option>
+              <option value="5">May</option>
+              <option value="6">June</option>
+              <option value="7">July</option>
+              <option value="8">August</option>
+              <option value="9">September</option>
+              <option value="10">October</option>
+              <option value="11">November</option>
+              <option value="12">December</option>
+            </select>
+          </div>
+          <div class="field">
+            <select class="ui fluid search dropdown" name="end-day">
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              <option value="5">5</option>
+              <option value="6">6</option>
+              <option value="7">7</option>
+              <option value="8">8</option>
+              <option value="9">9</option>
+              <option value="10">10</option>
+              <option value="11">11</option>
+              <option value="12">12</option>
+              <option value="13">13</option>
+              <option value="14">14</option>
+              <option value="15">15</option>
+              <option value="16">16</option>
+              <option value="17">17</option>
+              <option value="19">19</option>
+              <option value="20">20</option>
+              <option value="21">21</option>
+              <option value="22">22</option>
+              <option value="23">23</option>
+              <option value="24">24</option>
+              <option value="25">25</option>
+              <option value="26">26</option>
+              <option value="27">27</option>
+              <option value="28">28</option>
+              <option value="29">29</option>
+              <option value="30">30</option>
+              <option value="31">31</option>
+            </select>
+          </div>
+        </div>
+      </div>
+      <div class="field">
+        <label>Event Details/Description</label>
+        <textarea placeholder="Event Description....." name="event-description"></textarea>
+      </div>
+      <input type="submit" value="Create Event" class="ui button" type="submit"></input>
+    </form>
+</div>
+
+END;
+
+
+?>
