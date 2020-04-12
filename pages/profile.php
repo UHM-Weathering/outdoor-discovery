2b899de15e2192a812df9ae9ed41443251c22132
Emily Pang <42831699+eppang@users.noreply.github.com>
2020-04-12 03:19:51 -1000

    Edited profile spacing

--- pages/profile.php
+++ pages/profile.php
@@ -10,8 +10,8 @@ $html = <<<END
 </style>
 <div class="ui container">
   <div class="ui grid">
-    <div class="six wide column">
-      <div class="ui card">
+    <div class="five wide column">
+      <div class="ui fluid card">
         <div class="image">
           <img src="../images/surf.svg">
         </div>
@@ -29,18 +29,13 @@ $html = <<<END
         </div>
       </div>
     </div>
-    <div class="ten wide column">
+    <div class="eleven wide column">
       <div class="ui segment">
         <h3>Account Info</h3>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lectus quam id leo in vitae turpis massa sed elementum. Fringilla urna porttitor rhoncus dolor purus non. Suscipit tellus mauris a diam maecenas. Quam adipiscing vitae proin sagittis nisl rhoncus. Tempor nec feugiat nisl pretium fusce id velit. Tortor consequat id porta nibh venenatis cras sed felis. Egestas congue quisque egestas diam in. Non enim praesent elementum facilisis leo vel fringilla. Nunc faucibus a pellentesque sit amet porttitor eget. Etiam tempor orci eu lobortis elementum nibh tellus molestie. Eget nulla facilisi etiam dignissim diam quis. Vel pretium lectus quam id leo. Sed faucibus turpis in eu mi bibendum neque egestas congue. Consequat semper viverra nam libero justo laoreet sit. Quisque egestas diam in arcu cursus euismod quis viverra. Sodales ut etiam sit amet nisl purus in mollis nunc. Neque laoreet suspendisse interdum consectetur libero id faucibus nisl tincidunt. Ultrices vitae auctor eu augue ut lectus arcu bibendum. Nunc id cursus metus aliquam eleifend mi. Augue neque gravida in fermentum. Viverra suspendisse potenti nullam ac tortor vitae purus faucibus. Sapien eget mi proin sed. Mi ipsum faucibus vitae aliquet nec ullamcorper sit. Sed cras ornare arcu dui vivamus. Congue eu consequat ac felis donec.</p>
       </div>
-    </div>
-    <div class="six wide column"></div>
-    <div class="ten wide column">
       <div class="ui segment">
         <h3>Registered Events</h3>
-
-
         <div class="ui relaxed list">
           <div class="item">
             <div class="ui grid">
@@ -86,8 +81,6 @@ $html = <<<END
     </div>
   </div>
 </div>
-
-
 END;
 
 ?>
