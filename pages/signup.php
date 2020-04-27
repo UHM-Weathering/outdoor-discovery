7011752a9f1383686237b2b51f078823fed252db
Emily Pang <42831699+eppang@users.noreply.github.com>
2020-04-26 21:29:41 -1000

    Shows password requirements needed in real time

--- pages/signup.php
+++ pages/signup.php
@@ -19,14 +19,37 @@ if (isset($_POST["action"]) && $_POST["action"] == "process") {
 	}
 }
 
+
+
 $html = <<<END
 
+<style>
+.valid {
+  color: green;
+}
+
+.valid:before {
+position: relative
+left: -35px;
+content: "✔";
+}
+
+.invalid {
+  color: red;
+}
+
+.invalid:before {
+  position: relative;
+  left: -35px;
+  content: "✖";
+}
+</style>
 
 <div class="ui text container">
   <h2 class="ui blue centered header">
     <div class="content">Sign Up</div>
   </h2>
-  <div class="ui segment">
+  <div class="ui segment" style="margin-bottom: 30px">
     <form class="ui large form" action="" method="post">
       <input type="hidden" name="action" value="process">
       <div class="field">
@@ -34,18 +57,18 @@ $html = <<<END
         <input type="text" name="email" placeholder="Email">
       </div>
       <div class="field">
-        <label>Password</label>
-        <input type="password" name="password" placeholder="Password">
+        <label for="psw">Password</label>
+        <input id="psw" type="password" name="password" placeholder="Password" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}">
       </div>
-      <div class="ui tiny message">
+      <div class="ui tiny message" id="message" style="display: none">
         <div class="header">
           Password must contain:
         </div>
         <ul class="ui list">
-          <li>At least 8 characters</li>
-          <li>At least 1 uppercase letter</li>
-          <li>At least 1 lowercase letter</li>
-          <li>At least 1 digit</li>
+          <li id="length" class="invalid">At least 8 characters</li>
+          <li id="capital" class="invalid">At least 1 uppercase letter</li>
+          <li id="lowercase" class="invalid">At least 1 lowercase letter</li>
+          <li id="number" class="invalid">At least 1 digit</li>
         </ul>
       </div>
       <div class="field">
@@ -59,7 +82,61 @@ $html = <<<END
   </div>
 </div>
 
+<script>
+let myInput = document.getElementById("psw");
+let length = document.getElementById("length");
+let capital = document.getElementById("capital");
+let lowercase = document.getElementById("lowercase");
+let number = document.getElementById("number");
+  
+myInput.onfocus = function() {
+  document.getElementById("message").style.display = "block";
+}
 
+myInput.onblur = function() {
+  document.getElementById("message").style.display = "none";
+}
+
+myInput.onkeyup = function() {
+  let lowerCaseLetters = /[a-z]/g;
+  if(myInput.value.match(lowerCaseLetters)) {  
+    lowercase.classList.remove("invalid");
+    lowercase.classList.add("valid");
+  } else {
+    lowercase.classList.remove("valid");
+    lowercase.classList.add("invalid");
+  }
+  
+  // Validate capital letters
+  var upperCaseLetters = /[A-Z]/g;
+  if(myInput.value.match(upperCaseLetters)) {  
+    capital.classList.remove("invalid");
+    capital.classList.add("valid");
+  } else {
+    capital.classList.remove("valid");
+    capital.classList.add("invalid");
+  }
+
+  // Validate numbers
+  var numbers = /[0-9]/g;
+  if(myInput.value.match(numbers)) {  
+    number.classList.remove("invalid");
+    number.classList.add("valid");
+  } else {
+    number.classList.remove("valid");
+    number.classList.add("invalid");
+  }
+  
+  // Validate length
+  if(myInput.value.length >= 8) {
+    length.classList.remove("invalid");
+    length.classList.add("valid");
+  } else {
+    length.classList.remove("valid");
+    length.classList.add("invalid");
+  }
+}
+</script>
 
 END;
 ?>
